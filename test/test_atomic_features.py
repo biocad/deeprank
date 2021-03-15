55362b1a510fb7e93524d1fb67cd48540bbaa1de
Coos Baakman <coos.baakman@radboudumc.nl>
2021-03-15 12:28:32 +0000

    test the output without an intermediary file

--- test/test_atomic_features.py
+++ test/test_atomic_features.py
@@ -1,9 +1,69 @@
+import logging
 import unittest
+from nose.tools import eq_
 
 import numpy as np
 import pkg_resources
 
+from deeprank.selection import select_interface
 from deeprank.features import AtomicFeature
+
+
+_log = logging.getLogger(__name__)
+
+
+def _close(what, value1, value2, **kwargs):
+    if not np.isclose(value1, value2, **kwargs):
+        raise AssertionError("{} not close: {} vs. {}".format(what, value1, value2))
+
+
+def _make_atom(res_name, chain, res_num, atom_name, atom_key):
+
+    info = []
+    for index, key in enumerate(atom_key.split(',')):
+        key = key.strip()
+        if key == 'name':
+            info.append(atom_name)
+
+        elif key == 'chainID':
+            info.append(chain)
+
+        elif key == 'resSeq':
+            info.append(res_num)
+
+        elif key == 'resName':
+            info.append(res_name)
+
+    return tuple(info)
+
+
+def _parse_ref(file_, atom_key):
+
+    pairs = []
+    total_vdw_energy = None
+    total_elec_energy = None
+
+    for line in file_:
+        if len(line.strip()) == 0 or line.startswith('#'):
+            continue
+
+        elif line.startswith("Total Evdw = "):
+            total_vdw_energy = float(line.split('=')[1])
+
+        elif line.startswith("Total Eelec = "):
+            total_elec_energy = float(line.split('=')[1])
+        else:
+            res_name1, chain1, res_num1, atom_name1, res_name2, chain2, res_num2, atom_name2, dist, elec_energy, vdw_energy = line.split('\t')
+
+            pairs.append((_make_atom(res_name1.strip(), chain1.strip(), int(res_num1), atom_name1.strip(), atom_key),
+                          _make_atom(res_name2.strip(), chain2.strip(), int(res_num2), atom_name2.strip(), atom_key),
+                          float(dist), float(elec_energy), float(vdw_energy)))
+
+    return pairs, total_vdw_energy, total_elec_energy
+
+
+def _format_atom(info):
+    return " ".join(["{}".format(x) for x in info])
 
 
 class TestAtomicFeature(unittest.TestCase):
@@ -16,9 +76,8 @@ class TestAtomicFeature(unittest.TestCase):
         # - comment the first line (E0=1)
         # - uncomment the last two lines (Total = ...)
         # - use the corresponding PDB file to test
-        REF = './1AK4/atomic_features/ref_1AK4_100w.dat'
-        pdb = './1AK4/atomic_features/1AK4_100w.pdb'
-        test_name = './1AK4/atomic_features/test_1AK4_100w.dat'
+        REF = 'test/1AK4/atomic_features/ref_1AK4_100w.dat'
+        pdb = 'test/1AK4/atomic_features/1AK4_100w.pdb'
 
         # get the force field included in deeprank
         # if another FF has been used to compute the ref
@@ -27,7 +86,7 @@ class TestAtomicFeature(unittest.TestCase):
             'deeprank.features', '') + '/forcefield/'
 
         # declare the feature calculator instance
-        atfeat = AtomicFeature(pdb,
+        atfeat = AtomicFeature(pdb, select_interface(pdb, 'A', 'B', atom_distance=8.5),
                                param_charge=FF + 'protein-allhdg5-4_new.top',
                                param_vdw=FF + 'protein-allhdg5-4_new.param',
                                patch_file=FF + 'patch.top')
@@ -35,107 +94,35 @@ class TestAtomicFeature(unittest.TestCase):
         atfeat.assign_parameters()
 
         # only compute the pair interactions here
-        atfeat.evaluate_pair_interaction(save_interactions=test_name)
+        total_elec_energy, total_vdw_energy = atfeat.evaluate_pair_interaction()
 
-        # read the files
-        f = open(REF)
-        ref = f.readlines()
-        ref = [r for r in ref if not r.startswith(
-            '#') and not r.startswith('Total') and len(r.split()) > 0]
-        f.close()
+        # read the ref
+        with open(REF, 'rt') as f:
+            ref_pairs, ref_total_vdw_energy, ref_total_elec_energy = _parse_ref(f, atfeat.atom_key)
 
-        f = open(REF)
-        ref_tot = f.readlines()
-        ref_tot = [r for r in ref_tot if r.startswith('Total')]
-        f.close()
+        # Calculate the same sums for the ref as the atomic feature class does:
+        ref_elec = {}
+        ref_vdw = {}
+        for ref_atom1, ref_atom2, dist, elec, vdw in ref_pairs:
+            ref_elec[ref_atom1] = ref_elec.get(ref_atom1, 0) + elec
+            ref_elec[ref_atom2] = ref_elec.get(ref_atom2, 0) + elec
 
-        # read the test
-        f = open(test_name)
-        test = f.readlines()
-        test = [
-            t for t in test if len(
-                t.split()) > 0 and not t.startswith('Total')]
-        f.close()
+            ref_vdw[ref_atom1] = ref_vdw.get(ref_atom1, 0) + elec
+            ref_vdw[ref_atom2] = ref_vdw.get(ref_atom2, 0) + elec
 
-        f = open(test_name)
-        test_tot = f.readlines()
-        test_tot = [t for t in test_tot if t.startswith('Total')]
-        f.close()
+        # Compare the values
+        _log.debug("{} ref atoms, {} coulomb atoms, {} vdwaals atoms"
+                   .format(len(ref_elec), len(atfeat.feature_data['coulomb']),
+                           len(atfeat.feature_data['vdwaals'])))
 
-        # compare files
-        nint = 0
-        for ltest, lref in zip(test, ref):
-            ltest = ltest.split()
-            lref = lref.split()
+        eq_(atfeat.feature_data['coulomb'].keys(), ref_elec.keys())
+        for ref_atom in ref_elec:
+            _close("{} electro energy data".format(_format_atom(ref_atom)), ref_elec[ref_atom], atfeat.feature_data['coulomb'][ref_atom], atol=1E-6)
 
-            at_test = ((ltest[0], ltest[1], ltest[2], ltest[3]),
-                       (ltest[4], ltest[5], ltest[6], ltest[7]))
-            at_ref = ((lref[1], lref[2], lref[0], lref[3]),
-                      (lref[5], lref[6], lref[4], lref[7]))
-            if not at_test == at_ref:
-                raise AssertionError()
+        eq_(atfeat.feature_data['vdwaals'].keys(), ref_vdw.keys())
+        for ref_atom in ref_vdw:
+            _close("{} vdwaals energy data".format(_format_atom(ref_atom)), ref_vdw[ref_atom], atfeat.feature_data['vdwaals'][ref_atom], atol=1E-6)
 
-            dtest = np.array(float(ltest[8]))
-            dref = np.array(float(lref[8]))
-            if not np.allclose(dtest, dref, rtol=1E-3, atol=1E-3):
-                raise AssertionError()
+        _close("total electro energy", total_elec_energy, ref_total_elec_energy)
+        _close("total vanderwaals energy", total_vdw_energy, ref_total_vdw_energy)
 
-            val_test = np.array(ltest[9:11]).astype('float64')
-            val_ref = np.array(lref[9:11]).astype('float64')
-            if not np.allclose(val_ref, val_test, atol=1E-6):
-                raise AssertionError()
-
-            nint += 1
-
-        Etest = np.array([float(test_tot[0].split()[3]),
-                          float(test_tot[1].split()[3])])
-        Eref = np.array([float(ref_tot[0].split()[3]),
-                         float(ref_tot[1].split()[3])])
-        if not np.allclose(Etest, Eref):
-            raise AssertionError()
-
-        # close the db
-        atfeat.sqldb._close()
-
-    # @staticmethod
-    # def test_atomic_zdock():
-
-    #     # in case you change the ref don't forget to:
-    #     # - comment the first line (E0=1)
-    #     # - uncomment the last two lines (Total = ...)
-    #     # - use the corresponding PDB file to test
-    #     #REF = './1AK4/atomic_features/ref_1AK4_100w.dat'
-    #     pdb = './2OUL/decoys/2OUL_1.pdb'
-    #     test_name = './2OUL/atomic_features/test_2OUL_1.dat'
-
-    #     # get the force field included in deeprank
-    #     # if another FF has been used to compute the ref
-    #     # change also this path to the correct one
-    #     FF = pkg_resources.resource_filename(
-    #         'deeprank.features', '') + '/forcefield/'
-
-    #     # declare the feature calculator instance
-    #     atfeat = AtomicFeature(pdb,
-    #                            param_charge=FF + 'protein-allhdg5-4_new.top',
-    #                            param_vdw=FF + 'protein-allhdg5-4_new.param',
-    #                            patch_file=FF + 'patch.top')
-    #     # assign parameters
-    #     atfeat.assign_parameters()
-
-    #     # only compute the pair interactions here
-    #     atfeat.evaluate_pair_interaction(save_interactions=test_name)
-
-    #     # make sure that the other properties are not crashing
-    #     atfeat.compute_coulomb_interchain_only(contact_only=True)
-    #     atfeat.compute_coulomb_interchain_only(contact_only=False)
-
-    #     # make sure that the other properties are not crashing
-    #     atfeat.compute_vdw_interchain_only(contact_only=True)
-    #     atfeat.compute_vdw_interchain_only(contact_only=False)
-
-    #     # close the db
-    #     atfeat.sqldb._close()
-
-
-if __name__ == '__main__':
-    unittest.main()
