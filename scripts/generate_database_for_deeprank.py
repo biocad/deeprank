2c1689e5c29db030d15b0de8ad5437b9f509890e
wlad111 <wlad962961@gmail.com>
2021-05-25 12:49:49 +0300

    multiprocessing via thread pool

--- scripts/generate_database_for_deeprank.py
+++ scripts/generate_database_for_deeprank.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from tqdm import tqdm
+from tqdm import *
 import shutil
-from mpi4py import MPI
+# from mpi4py import MPI
+from multiprocessing import Pool
 
-comm = MPI.COMM_WORLD
+# comm = MPI.COMM_WORLD
 
 from deeprank.generate import *
 
@@ -13,21 +14,47 @@ NATIVE_SUBDIR = 'native'
 
 input_data_path = sys.argv[1]
 hdf5_out_dir = sys.argv[2]
+POOL_SIZE = int(sys.argv[3])
 
-# if os.path.exists(hdf5_out_dir):
-#     shutil.rmtree(hdf5_out_dir)
+if os.path.exists(hdf5_out_dir):
+    shutil.rmtree(hdf5_out_dir)
+
+if not os.path.exists(hdf5_out_dir):
+    os.makedirs(hdf5_out_dir)
 
 def parse_complex_name(cplx_name):
     pdb_id, names = cplx_name.split('_')
     first_names, second_names = names.split('|')
-    chains1, chains2 = first_names.split('+'), second_names.split('+')
-    return chains1, chains2
+    ch1, ch2 = first_names.split('+'), second_names.split('+')
+    return ch1, ch2
 
 
-# if not os.path.exists(hdf5_out_dir):
-#     os.makedirs(hdf5_out_dir)
+# for i, cplx in enumerate(tqdm(os.listdir(input_data_path), total=len(os.listdir(input_data_path)))):
+#     try:
+#         chains1, chains2 = parse_complex_name(cplx)
+#         # TBD generate features
+#         complex_data = DataGenerator(pdb_source=os.path.join(input_data_path, cplx, DECOY_SUBDIR),
+#                                      pdb_native=os.path.join(input_data_path, cplx, NATIVE_SUBDIR),
+#                                      pssm_source='.',
+#                                      compute_features=['deeprank.features.AtomicFeature',
+#                                                        'deeprank.features.ResidueDensity',
+#                                                        'deeprank.features.BSA'],
+#                                      compute_targets=['deeprank.targets.binary_class'],
+#                                      chain1=chains1,
+#                                      chain2=chains2,
+#                                      hdf5=os.path.join(hdf5_out_dir, f"{cplx}.hdf5"),
+#                                      mpi_comm=comm)
+#         complex_data.create_database(prog_bar=True)
+#         grid_info = {'number_of_points': [30, 30, 30],
+#                      'resolution': [1., 1., 1.],
+#                      'atomic_densities': {'C': 1.7, 'N': 1.55, 'O': 1.52, 'S': 1.8}
+#                     }
+#         complex_data.map_features(grid_info, try_sparse=True, prog_bar=True)
+#         print(f"generated for {cplx} [{i+1}/{len(os.listdir(input_data_path))}]")
+#     except Exception:
+#         continue
 
-for i, cplx in enumerate(tqdm(os.listdir(input_data_path), total=len(os.listdir(input_data_path)))):
+def calculate(cplx):
     try:
         chains1, chains2 = parse_complex_name(cplx)
         # TBD generate features
@@ -41,15 +68,23 @@ for i, cplx in enumerate(tqdm(os.listdir(input_data_path), total=len(os.listdir(
                                      chain1=chains1,
                                      chain2=chains2,
                                      hdf5=os.path.join(hdf5_out_dir, f"{cplx}.hdf5"),
-                                     mpi_comm=comm)
-        complex_data.create_database(prog_bar=True)
+                                     data_augmentation=5)
+        complex_data.create_database(prog_bar=False)
         grid_info = {'number_of_points': [30, 30, 30],
                      'resolution': [1., 1., 1.],
                      'atomic_densities': {'C': 1.7, 'N': 1.55, 'O': 1.52, 'S': 1.8}
-                    }
-        complex_data.map_features(grid_info, try_sparse=True, prog_bar=True)
-        print(f"generated for {cplx} [{i+1}/{len(os.listdir(input_data_path))}]")
+                     }
+        complex_data.map_features(grid_info, try_sparse=True, prog_bar=False)
     except Exception:
-        continue
+        pass
 
 
+complexes_list = os.listdir(input_data_path)
+
+results = []
+with Pool(POOL_SIZE) as p:
+    max_ = len(complexes_list)
+    with tqdm(total=max_) as pbar:
+        for r in p.imap_unordered(calculate, complexes_list):
+            results.append(r)
+            pbar.update()
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -8,17 +8,36 @@ import numpy as np
 DATABASE_PATH = ""
 
 path_to_hdf5_complexes = sys.argv[1]
+path_to_raw_complexes = sys.argv[2]
+
+
+def parse_complex_name(cplx_name):
+    pdb_id, names = cplx_name.split('_')
+    first_names, second_names = names.split('|')
+    chains1, chains2 = first_names.split('+'), second_names.split('+')
+    return chains1, chains2
+
 
 database = [os.path.join(path_to_hdf5_complexes, f) for f in os.listdir(path_to_hdf5_complexes)]
+complexes_names = [dirname for dirname in os.listdir(path_to_raw_complexes)]
+
+chains_dict = {cplx : (parse_complex_name(cplx)) for cplx in complexes_names}
+
+grid_info = {'number_of_points': [30, 30, 30],
+                     'resolution': [1., 1., 1.],
+                     'atomic_densities': {'C': 1.7, 'N': 1.55, 'O': 1.52, 'S': 1.8}
+                     }
 
 data_set = DataSet(train_database=database,
                    valid_database=None,
                    test_database=None,
                    grid_shape=(30, 30, 30),
+                   grid_info=grid_info,
                    select_target='BIN_CLASS',
                    normalize_features=True,
                    normalize_targets=False,
                    pair_chain_feature=np.add,
+                   chain_mappings=chains_dict,
                    mapfly=False)
 
 model = NeuralNet(data_set=data_set,
@@ -30,4 +49,3 @@ model.train(nepoch=5,
             train_batch_size=5,
             num_workers=1,
             hdf5='train_example')
-
