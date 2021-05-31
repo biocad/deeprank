5f92b678434b696c93c88e23ccf5fb4291a2adcc
wlad111 <wlad962961@gmail.com>
2021-05-31 13:00:51 +0300

    add data augmentation as command line argument

--- scripts/generate_database_for_deeprank.py
+++ scripts/generate_database_for_deeprank.py
@@ -2,10 +2,8 @@ import sys
 import os
 from tqdm import *
 import shutil
-# from mpi4py import MPI
 from multiprocessing import Pool
 
-# comm = MPI.COMM_WORLD
 
 from deeprank.generate import *
 
@@ -15,6 +13,9 @@ NATIVE_SUBDIR = 'native'
 input_data_path = sys.argv[1]
 hdf5_out_dir = sys.argv[2]
 POOL_SIZE = int(sys.argv[3])
+ROTATIONS = int(sys.argv[4])
+
+augmentation = None if ROTATIONS == 0 else ROTATIONS
 
 if os.path.exists(hdf5_out_dir):
     shutil.rmtree(hdf5_out_dir)
@@ -28,36 +29,9 @@ def parse_complex_name(cplx_name):
     ch1, ch2 = first_names.split('+'), second_names.split('+')
     return ch1, ch2
 
-
-# for i, cplx in enumerate(tqdm(os.listdir(input_data_path), total=len(os.listdir(input_data_path)))):
-#     try:
-#         chains1, chains2 = parse_complex_name(cplx)
-#         # TBD generate features
-#         complex_data = DataGenerator(pdb_source=os.path.join(input_data_path, cplx, DECOY_SUBDIR),
-#                                      pdb_native=os.path.join(input_data_path, cplx, NATIVE_SUBDIR),
-#                                      pssm_source='.',
-#                                      compute_features=['deeprank.features.AtomicFeature',
-#                                                        'deeprank.features.ResidueDensity',
-#                                                        'deeprank.features.BSA'],
-#                                      compute_targets=['deeprank.targets.binary_class'],
-#                                      chain1=chains1,
-#                                      chain2=chains2,
-#                                      hdf5=os.path.join(hdf5_out_dir, f"{cplx}.hdf5"),
-#                                      mpi_comm=comm)
-#         complex_data.create_database(prog_bar=True)
-#         grid_info = {'number_of_points': [30, 30, 30],
-#                      'resolution': [1., 1., 1.],
-#                      'atomic_densities': {'C': 1.7, 'N': 1.55, 'O': 1.52, 'S': 1.8}
-#                     }
-#         complex_data.map_features(grid_info, try_sparse=True, prog_bar=True)
-#         print(f"generated for {cplx} [{i+1}/{len(os.listdir(input_data_path))}]")
-#     except Exception:
-#         continue
-
 def calculate(cplx):
     try:
         chains1, chains2 = parse_complex_name(cplx)
-        # TBD generate features
         complex_data = DataGenerator(pdb_source=os.path.join(input_data_path, cplx, DECOY_SUBDIR),
                                      pdb_native=os.path.join(input_data_path, cplx, NATIVE_SUBDIR),
                                      pssm_source='.',
@@ -67,8 +41,8 @@ def calculate(cplx):
                                      compute_targets=['deeprank.targets.binary_class'],
                                      chain1=chains1,
                                      chain2=chains2,
-                                     hdf5=os.path.join(hdf5_out_dir, f"{cplx}.hdf5"))
-                                     # data_augmentation=5)
+                                     hdf5=os.path.join(hdf5_out_dir, f"{cplx}.hdf5"),
+                                     data_augmentation=augmentation)
         complex_data.create_database(prog_bar=False)
         grid_info = {'number_of_points': [30, 30, 30],
                      'resolution': [1., 1., 1.],
