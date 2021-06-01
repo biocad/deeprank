399cd2692c8f03f3f92eb5812ac736f3b04ce035
wlad111 <wlad962961@gmail.com>
2021-06-01 16:35:04 +0300

    generate test dataset

---
+++ scripts/generate_test_set.py
@@ -0,0 +1,14 @@
+import random
+import sys
+import os
+
+path_to_hdf5_complexes = sys.argv[1]
+path_to_raw_complexes = sys.argv[2]
+test_size = sys.argv[4]
+
+complexes_names = [dirname for dirname in os.listdir(path_to_raw_complexes)]
+
+test_complexes_names = random.sample(complexes_names, test_size)
+
+with open('test_complexes.txt', 'w') as f:
+    f.writelines(test_complexes_names)
