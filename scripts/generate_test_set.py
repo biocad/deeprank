3ce4a3c082e3078afaabf673730b969fbbacde32
wlad111 <wlad962961@gmail.com>
2021-06-01 16:57:47 +0300

    generate test dataset #4

--- scripts/generate_test_set.py
+++ scripts/generate_test_set.py
@@ -10,4 +10,5 @@ complexes_names = [dirname for dirname in os.listdir(path_to_raw_complexes)]
 test_complexes_names = random.sample(complexes_names, int(test_size * len(complexes_names)))
 
 with open('test_complexes.txt', 'w') as f:
-    f.writelines(test_complexes_names)
+    for line in test_complexes_names:
+        f.write(line)
